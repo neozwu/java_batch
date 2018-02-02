102ac119051b9ae94a4663b9e67170f08efff5f1
Neo Wu <neowu@google.com>
2018-02-02 13:45:20 -0800

    format

--- java_batch_gen.py
+++ java_batch_gen.py
@@ -11,18 +11,47 @@ from google.protobuf import json_format
 import yaml
 import shutil
 
-
 APIS = [
-# shared package
-        'core', 'appengine', 'iam', 
-# gapic
-        'bigquerydatatransfer', 'bigtable', 'bigtableadmin', 'container', 'dataproc_v1', 'datastore', 'dialogflow_v2beta1_java',
-'dlp_v2beta1', 'errorreporting', 'firestore', 'language_v1', 'language_v1beta2', 'logging', 'longrunning', 'monitoring', 'pubsub', 'oslogin_v1', 'spanner', 'spanner_admin_database', 'spanner_admin_instance',
-'speech_v1', 'speech_v1beta1', 'cloudtrace_v1', 'cloudtrace_v2', 'videointelligence_v1beta1', 'videointelligence_v1beta2', 'videointelligence_v1', 'vision_v1', 'vision_v1p1beta1']
+    # shared package
+    'core',
+    'appengine',
+    'iam',
+    # gapic API
+    'bigquerydatatransfer',
+    'bigtable',
+    'bigtableadmin',
+    'container',
+    'dataproc_v1',
+    'datastore',
+    'dialogflow_v2beta1_java',
+    'dlp_v2beta1',
+    'dlp_v2beta2',
+    'errorreporting',
+    'firestore',
+    'language_v1',
+    'language_v1beta2',
+    'logging',
+    'longrunning',
+    'monitoring',
+    'pubsub',
+    'oslogin_v1',
+    'spanner',
+    'spanner_admin_database',
+    'spanner_admin_instance',
+    'speech_v1',
+    'speech_v1beta1',
+    'cloudtrace_v1',
+    'cloudtrace_v2',
+    'videointelligence_v1beta1',
+    'videointelligence_v1beta2',
+    'videointelligence_v1',
+    'vision_v1',
+    'vision_v1p1beta1',
+]
 
 PROTO_EXCLUSION = ['longrunning']
 GRPC_EXCLUSION = ['appengine', 'longrunning']
-
+COPY_EXCLUSION = ['dlp_v2beta2']
 
 def get_artman_yaml(googleapis_repo):
   artman_yaml_files = []
@@ -56,7 +85,8 @@ def get_task_type(artman_yaml_file):
     return 'JAVA_GRPC'
 
 
-def run_batch(api, artman_yaml,
+def run_batch(api,
+              artman_yaml,
               root_dir,
               staging_repo,
               docker_mode,
@@ -68,20 +98,20 @@ def run_batch(api, artman_yaml,
     return _run_java_gapic(api, artman_yaml, root_dir, staging_repo,
                            docker_mode, g3artman_mode, dry_run)
   elif task_type == 'JAVA_GRPC':
-    return _run_java_grpc(api, artman_yaml, root_dir, staging_repo,
-                          docker_mode, g3artman_mode, dry_run)
+    return _run_java_grpc(api, artman_yaml, root_dir, staging_repo, docker_mode,
+                          g3artman_mode, dry_run)
   else:
     return False
 
 
-def _run_java_gapic(api, artman_yaml, root_dir, local_staging_repo,
-                    docker_mode, g3artman_mode, dry_run):
+def _run_java_gapic(api, artman_yaml, root_dir, local_staging_repo, docker_mode,
+                    g3artman_mode, dry_run):
   cmd = ('%s %s --config %s --root-dir %s publish --local-repo-dir %s '
          '--dry-run --target staging java_gapic') % (
              'artman' if not g3artman_mode else 'g3artman', '--local'
              if not docker_mode else '', artman_yaml, root_dir,
              local_staging_repo)
-  print('JAVA_BATCH:  running: %s' % cmd)
+  print('JAVA_BATCH>  running: %s' % cmd)
   if not dry_run:
     try:
       subprocess.check_output(cmd, shell=True)
@@ -96,8 +126,7 @@ def _run_java_grpc(api, artman_yaml, root_dir, local_staging_repo, docker_mode,
       '%s %s --config %s --root-dir %s publish --local-repo-dir %s '
       '--dry-run --target staging java_proto') % (
           'artman' if not g3artman_mode else 'g3artman', '--local'
-          if not docker_mode else '', artman_yaml, root_dir,
-          local_staging_repo)
+          if not docker_mode else '', artman_yaml, root_dir, local_staging_repo)
 
   java_grpc_cmd = (
       'artman %s --config %s --root-dir %s publish --local-repo-dir %s '
@@ -107,7 +136,7 @@ def _run_java_grpc(api, artman_yaml, root_dir, local_staging_repo, docker_mode,
                                                  local_staging_repo)
 
   if api not in PROTO_EXCLUSION:
-    print('JAVA_BATCH: running: %s' % java_proto_cmd)
+    print('JAVA_BATCH> running: %s' % java_proto_cmd)
     if not dry_run:
       try:
         subprocess.check_output(java_proto_cmd, shell=True)
@@ -115,7 +144,7 @@ def _run_java_grpc(api, artman_yaml, root_dir, local_staging_repo, docker_mode,
         return False
 
   if api not in GRPC_EXCLUSION:
-    print('JAVA_BATCH: running: %s' % java_grpc_cmd)
+    print('JAVA_BATCH> running: %s' % java_grpc_cmd)
     if not dry_run:
       try:
         subprocess.check_output(java_grpc_cmd, shell=True)
@@ -139,8 +168,14 @@ def _parse_args(*args):
       type=str,
       default='../api-client-staging',
       help=
-      'api-client-staging repo directory. Default to \'../api-client-staging\''
-  )
+      'api-client-staging repo directory. Default to \'../api-client-staging\'')
+
+  parser.add_argument(
+      '--gcj-repo-dir',
+      type=str,
+      default='',
+      help=
+      'google-cloud-java repo directory. If this argument is set, batch script will run copy task only')
 
   parser.add_argument(
       '--api-list',
@@ -201,7 +236,7 @@ def _parse_args(*args):
       flags.root_dir = os.path.join(user_config['local_paths']['reporoot'],
                                     'googleapis')
     else:
-      print('JAVA_BATCH: fatal error: `--root_dir` or '
+      print('JAVA_BATCH> fatal error: `--root_dir` or '
             '`googleapis` field in artman user config must be specified.')
       sys.exit(1)
 
@@ -212,32 +247,36 @@ def _parse_args(*args):
 def _get_config_path_relative_to_googleapis(path):
   return path[path.find('googleapis/') + 11:]
 
+
 def remove_proto_exclusion(flags, mapping):
   for api in PROTO_EXCLUSION:
     artman_yaml = mapping[api]
     proto_dir = _get_staging_dir(artman_yaml, 'java_gapic', 'proto')
     if proto_dir:
-      print("JAVA_BATCH: deleting: " + proto_dir)
+      print('JAVA_BATCH> deleting: ' + proto_dir)
       shutil.rmtree(proto_dir)
+
 
 def remove_grpc_exclusion(flags, mapping):
   for api in GRPC_EXCLUSION:
     artman_yaml = mapping[api]
     grpc_dir = _get_staging_dir(artman_yaml, 'java_gapic', 'grpc')
     if grpc_dir:
-      print("JAVA_BATCH: deleting: " + grpc_dir)
+      print('JAVA_BATCH> deleting: ' + grpc_dir)
       shutil.rmtree(grpc_dir)
+
 
 def _get_staging_dir(artman_yaml, artifact_name, dir_mapping_name):
   artman_config = _get_artman_config(artman_yaml)
   for artifact in artman_config.artifacts:
     if artifact.name == artifact_name:
       for publish_target in artifact.publish_targets:
-        if publish_target.name == "staging":
+        if publish_target.name == 'staging':
           for dir_mapping in publish_target.directory_mappings:
             if dir_mapping.name == dir_mapping_name:
               return dir_mapping.dest
   return None
+
 
 def _get_artman_config(artman_yaml):
   config_pb = Config()
@@ -254,21 +293,24 @@ def main(*args):
   mapping = api_to_yaml_mapping(get_artman_yaml(flags.root_dir))
   print(mapping)
 
-  if flags.exclude:
-      for api in flags.exclude.split(','):
-          APIS.remove(api)
-
-  if flags.api_list:
-    for api in flags.api_list.split(','):
-      run_batch(api, mapping[api], flags.root_dir, flags.local_repo_dir,
-              flags.docker_mode, flags.g3artman_mode, flags.dryrun_mode)
+  if flags.gcj_repo_dir:
+    pass
   else:
-    for api in APIS:
-      run_batch(api, mapping[api], flags.root_dir, flags.local_repo_dir,
-              flags.docker_mode, flags.g3artman_mode, flags.dryrun_mode)
+    if flags.exclude:
+      for api in flags.exclude.split(','):
+        APIS.remove(api)
 
-  remove_proto_exclusion(flags, mapping)
-  remove_grpc_exclusion(flags, mapping)
+    if flags.api_list:
+      for api in flags.api_list.split(','):
+        run_batch(api, mapping[api], flags.root_dir, flags.local_repo_dir,
+                  flags.docker_mode, flags.g3artman_mode, flags.dryrun_mode)
+    else:
+      for api in APIS:
+        run_batch(api, mapping[api], flags.root_dir, flags.local_repo_dir,
+                  flags.docker_mode, flags.g3artman_mode, flags.dryrun_mode)
+
+    remove_proto_exclusion(flags, mapping)
+    remove_grpc_exclusion(flags, mapping)
 
 if __name__ == '__main__':
   main()
